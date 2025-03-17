4117de44c30838b18d641d575b65b8d2779c88a7
Daisy Kirui <97473233+DaisyKirui@users.noreply.github.com>
2025-03-17 18:02:50 +0300

    new code

---
+++ first.r
@@ -0,0 +1,35 @@
+print ("Daisy")
+#variable declaration
+#legal ways of writing variables
+# Assigning a value to a variable
+myvar <- "hello world"
+
+# Assigning a numeric value
+numvar <- 42
+
+# Assigning a logical value
+boolvar <- TRUE
+
+# Assigning a vector
+vecvar <- c(1, 2, 3, 4, 5)
+
+
+#pinting a variable
+print (myvar)
+print (vecvar)
+
+#string length
+print(nchar(myvar))
+
+#string concatenation
+name <- "Daisy"
+greeting <- "Hi"
+
+#print (paste(greeting, name))
+
+cat("  My greeting is:",greeting)
+#different ways of printing
+#print
+#cat
+#paste
+#name of variable
